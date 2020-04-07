011e2d9c5093a2312029ced7100a71f49997e24e
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-04-07 22:35:51 +0100

    Challenge 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const reportedCases = data;
+
+    return {
+        data: reportedCases,
+        impact: {
+            currentlyInfected: reportedCases * 10,
+            infectionsByRequestedTime: currentlyInfected * 512
+        },
+        severeImpact: {
+            currentlyInfected: reportedCases * 50,
+            infectionsByRequestedTime: currentlyInfected * 512
+        }
+    }
+}
 
 export default covid19ImpactEstimator;
