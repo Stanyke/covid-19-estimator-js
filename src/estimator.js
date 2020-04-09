216f73d042c4dad4c640fb89d0c1dbe116c1e6b0
Stanley Chibuike <42142189+Stanyke@users.noreply.github.com>
2020-04-09 06:10:07 +0100

    Challenge 1 update

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,17 @@
 const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
-  
-  const numberOfDays;
-  
-  if (data.periodType === 'days')
-  {
+
+  let numberOfDays;
+  if (data.periodType === 'days') {
     numberOfDays = data.timeToElapse;
-  }
-  else if (data.periodType === 'weeks')
-  {
+  } else if (data.periodType === 'weeks') {
     numberOfDays = 7 * data.timeToElapse;
   }
-  if (data.periodType === 'months')
-  {
+  if (data.periodType === 'months') {
     numberOfDays = 30 * data.timeToElapse;
   }
-  
+
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
 
