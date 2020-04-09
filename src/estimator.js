113afa88ec87ae51a1c7cd6173c805faebb1b61d
Stanley Chibuike <42142189+Stanyke@users.noreply.github.com>
2020-04-09 06:00:45 +0100

    Challenge 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,32 @@
 const covid19ImpactEstimator = (data) => {
-  const reportedCases = data;
-  const impactCurrentlyInfected = reportedCases * 10;
-  const severeImpactcurrentlyInfected = reportedCases * 50;
-  const impactInfectionsByRequestedTime = impactCurrentlyInfected * 512;
-  const severeImpactInfectionsByRequestedTime = severeImpactcurrentlyInfected * 512;
-  return {
-    data: reportedCases,
-    impact: {
-      currentlyInfected: impactCurrentlyInfected,
-      infectionsByRequestedTime: {
-        days: impactInfectionsByRequestedTime
-      }
-    },
-    severeImpact: {
-      currentlyInfected: severeImpactcurrentlyInfected,
-      infectionsByRequestedTime: {
-        days: severeImpactInfectionsByRequestedTime
-      }
-    }
-  };
-};
+  const impact = {};
+  const severeImpact = {};
+  
+  const numberOfDays;
+  
+  if (data.periodType === 'days')
+  {
+    numberOfDays = data.timeToElapse;
+  }
+  else if (data.periodType === 'weeks')
+  {
+    numberOfDays = 7 * data.timeToElapse;
+  }
+  if (data.periodType === 'months')
+  {
+    numberOfDays = 30 * data.timeToElapse;
+  }
+  
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  const exponent = Math.floor(numberOfDays / 3);
+
+  impact.infectionsByRequestedTime = Math.floor(
+    impact.currentlyInfected * 2 ** exponent
+  );
+
+  severeImpact.infectionsByRequestedTime = Math.floor(
+    severeImpact.currentlyInfected * 2 ** exponent
+  );
 export default covid19ImpactEstimator;
