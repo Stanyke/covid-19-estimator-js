e9962f7ebef1a0f0b8f44f9bc1d6e773448a716b
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-04-12 13:14:31 +0100

    Updated API Routes

--- src/routes.js
+++ src/routes.js
@@ -177,7 +177,7 @@ app.get('/api/v1/on-covid-19/logs', (req, res) => {
 
       const apiRunTimeSpent = `${(apiRunTimeEnd - apiRunTimeBegin)}ms`;
 
-      fs.appendFile(logsFilePath, `GET  /api/v1/on-covid-19/logs  200  ${apiRunTimeSpent}\n`, (fsErrv) => {
+      fs.appendFile(logsFilePath, `GET  /api/v1/on-covid-19/logs  200  ${apiRunTimeSpent}\n`, (fsErr) => {
         if (fsErr) throw err;
       });
     }
