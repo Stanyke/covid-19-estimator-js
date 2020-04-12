3700434c06844cf400555bf7a58128b9740b4e0a
Stanley Chibuike <42142189+Stanyke@users.noreply.github.com>
2020-04-12 17:28:58 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -35,12 +35,7 @@ app.post('/api/v1/on-covid-19', (req, res) => {
   const apiRunTimeBegin = new Date().getTime();
 
   const data = {};
-  data.region = {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  };
+  data.region = req.body.region;
   data.periodType = req.body.periodType;
   data.timeToElapse = req.body.timeToElapse;
   data.reportedCases = req.body.reportedCases;
